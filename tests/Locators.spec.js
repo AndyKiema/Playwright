f74542ac88e69342fde46b7dea4e4a67ef05a85a
Andrew Kiema <andrewkiema94@gmail.com>
2025-09-04 07:04:00 +0300

    Add multiple locators

--- tests/Locators.spec.js
+++ tests/Locators.spec.js
@@ -1,8 +1,11 @@
 const {test,expect}=require('@playwright/test');
 
-test('Locators with CSS',async({page})=>{
+test('Locators',async({page})=>{
   await page.goto('https://demoblaze.com/');
   await page.click('id=login2');//Click on login button
   await page.fill('#loginusername','andrewk');//Input username
   await page.fill('#loginpassword','mypassword'); //Input password
+  await page.click("//button[normalize-space()='Log in']");
+  const logOut=await page.locator("(//a[normalize-space()='Log out'])[1]");
+  await expect(logOut).toBeVisible();
 })
