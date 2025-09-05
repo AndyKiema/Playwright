1cd408b1b6ec58a8474eb079f61ef72162b933c7
Andrew Kiema <andrewkiema94@gmail.com>
2025-09-05 07:09:52 +0300

    Add notes

--- tests/locatorsBuiltin.spec.js
+++ tests/locatorsBuiltin.spec.js
@@ -3,17 +3,17 @@ const {test,expect}=require('@playwright/test');
 test('Built-in Locators',async({page})=>{
   await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
   const logo=await page.getByAltText('company-branding'); //locate an image using its text alternative
-  await expect (logo).toBeVisible();
+  await expect (logo).toBeVisible()
 
-  const usernameField= await page.getByPlaceholder('Username');
+  const usernameField= await page.getByPlaceholder('Username'); //Locate username field
   await usernameField.fill('Admin');
 
-  const passwordField=await page.getByPlaceholder('Password');
+  const passwordField=await page.getByPlaceholder('Password'); //Locate password field
   await passwordField.fill('admin123');
 
-  await page.getByRole('button',{type:'submit'}).click();
+  await page.getByRole('button',{type:'submit'}).click(); //Click submit button
 
-  const name=await page.locator("//p[@class='oxd-userdropdown-name']");
+  const name=await page.locator("//p[@class='oxd-userdropdown-name']"); //Locate name next to avatar
 
   console.log(await name.textContent());
   await expect(name).toBeVisible();
